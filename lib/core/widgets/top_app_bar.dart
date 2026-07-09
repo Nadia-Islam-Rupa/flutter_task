ccc913c3be4f4dbeda2de689cffbea08fd7384cf
Nadia Islam <110677694+Nadia-Islam-Rupa@users.noreply.github.com>
2026-07-09 22:40:12 +0600

    add AppTopBar widget for a customizable top app bar with navigation and action buttons

---
+++ lib/core/widgets/top_app_bar.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_task/core/theme/app_style.dart';
+import 'package:flutter_task/core/widgets/icon_button_circle.dart';
+
+class AppTopBar extends StatelessWidget {
+  const AppTopBar({
+    super.key,
+    required this.title,
+    this.onBack,
+    this.onShare,
+    this.onNotifications,
+  });
+
+  final String title;
+  final VoidCallback? onBack;
+  final VoidCallback? onShare;
+  final VoidCallback? onNotifications;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        CircleIconButton(icon: Icons.chevron_left, onTap: onBack),
+        Expanded(
+          child: Text(
+            title,
+            textAlign: TextAlign.center,
+            style: AppTextStyles.appBarTitle,
+          ),
+        ),
+        CircleIconButton(icon: Icons.ios_share_outlined, onTap: onShare),
+        const SizedBox(width: 10),
+        CircleIconButton(
+          icon: Icons.notifications_none_rounded,
+          onTap: onNotifications,
+        ),
+      ],
+    );
+  }
+}
