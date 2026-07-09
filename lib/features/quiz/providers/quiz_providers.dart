01b60b200f9191ca3cff49ba29112c44ea4b4117
Nadia Islam <110677694+Nadia-Islam-Rupa@users.noreply.github.com>
2026-07-09 23:23:37 +0600

    add HomeScreen widget for the main application interface

--- lib/features/quiz/providers/quiz_providers.dart
+++ lib/features/quiz/providers/quiz_providers.dart
@@ -1,6 +1,5 @@
 import 'dart:async';
 
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_riverpod/legacy.dart';
 
 /// Letter key ('A'..'E') of the option currently selected on the Flashcard
